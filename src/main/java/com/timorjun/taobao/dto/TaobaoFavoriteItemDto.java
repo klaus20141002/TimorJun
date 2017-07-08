7e32263bc140e97310e5062df7d2d2f0ee4b152f
Niklaus Mikaelson <love20141002@126.com>
2017-07-08 17:41:30 +0800

    -- handle license issue

--- src/main/java/com/timorjun/taobao/dto/TaobaoFavoriteItemDto.java
+++ src/main/java/com/timorjun/taobao/dto/TaobaoFavoriteItemDto.java
@@ -6,7 +6,7 @@
  * @Date        :      2017年7月8日
  *
  * @Copyright Notice: 
- * Copyright (c) 2014 TimorJun, Inc. All  Rights Reserved.
+ * Copyright (c) 2017 TimorJun, Inc. All  Rights Reserved.
  * This software is published under the terms of TimorJun
  * License version 1.0, a copy of which has been included with this
  * distribution in the LICENSE.txt file.
